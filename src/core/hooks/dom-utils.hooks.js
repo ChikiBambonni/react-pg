835de6a80414cc726e2e75525fa8eda7414818e1
ChikiBambonni <7exalted7@gmail.com>
2020-04-18 22:55:08 +0300

    create useOutsideClick hook

--- src/core/hooks/dom-utils.hooks.js
+++ src/core/hooks/dom-utils.hooks.js
@@ -14,3 +14,18 @@ export const useSize = ref => {
     width
   }
 };
+
+export const useOutsideClick = (ref, callback) => {
+  useEffect(() => {
+    const handleClickOutside = event => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
--- src/shared/common-table/column-filters/column-filter.component.js
+++ src/shared/common-table/column-filters/column-filter.component.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 // import PropTypes from 'prop-types';
 import Paper from "@material-ui/core/Paper";
 import FilterListIcon from '@material-ui/icons/FilterList';
 
+import { useOutsideClick } from "@core/hooks";
 import { useStyles } from "./column-filter.styles";
 
 export const ColumnFilters = () => {
   const classes = useStyles();
 
   const [isExpanded, setIsExpanded] = useState(false);
-
   const toggleExpanded = () => setIsExpanded(!isExpanded);
+
+  const filtersWrapperRef = useRef(null);
+  useOutsideClick(filtersWrapperRef, setIsExpanded);
 
   return (
     <div>
@@ -20,11 +23,16 @@ export const ColumnFilters = () => {
           onClick={toggleExpanded}
         />
       </div>
-      {isExpanded && <div className={classes.filtersWrapper}>
-        <Paper style={{ minWidth: 220 }}>
-          ColumnFilters works!
-        </Paper>
-      </div>}
+      {isExpanded && 
+        <div 
+          className={classes.filtersWrapper}
+          ref={filtersWrapperRef}
+        >
+          <Paper style={{ minWidth: 220 }}>
+            ColumnFilters works!
+          </Paper>
+        </div>
+      }
     </div>
   );
 };
