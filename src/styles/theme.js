df2910c207583c666e74d33aca3cba6b79de97eb
ChikiBambonni <7exalted7@gmail.com>
2020-04-18 18:40:14 +0300

    change theme colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,26 +1,26 @@
 import { createMuiTheme } from '@material-ui/core/styles';  
 
-import { spacing } from './variables';
+import { 
+  spacing,
+  primaryColors,
+  secondaryColors,
+  errorColors,
+  warningColors,
+  infoColors,
+  successColors,
+  textColors
+} from './variables';
 
 export const theme = createMuiTheme({
   spacing,
   palette: {
-    primary: {
-      light: "#222A45",
-      main: "#24292e",
-      contrastText: "#ffffff"
-    },
-    secondary: {
-      main: "#7467ef",
-    },
-    warning: {
-      main: "#ff9e43"
-    },
-    text: {
-      primary: "rgba(255, 255, 255, 1)",
-      secondary: "rgba(255, 255, 255, 0.7)",
-      disabled: "rgba(255, 255, 255, 0.2)"
-    }
+    primary: primaryColors,
+    secondary: secondaryColors,
+    warning: warningColors,
+    error: errorColors,
+    success: successColors,
+    info: infoColors,
+    text: textColors
   },
   overrides: {
     MuiPaper: {
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,3 +1,45 @@
 export const spacing = 4;
 
 export const warning$1 = "#ff9e43";
+
+export const primaryColors = {
+  light: "#30333e",
+  main: "#171d30",
+  dark: "#101421"
+};
+
+export const secondaryColors = {
+  light: "#8f85f2",
+  main: "#7467ef",
+  dark: "#5148a7"
+};
+
+export const errorColors = {
+  light: "#aa2e25",
+  main: "#f44336",
+  dark: "#f6685e"
+};
+
+export const warningColors = {
+  light: "#ffb168",
+  main: "#ff9e43",
+  dark: "#b26e2e"
+};
+
+export const infoColors = {
+  light: "#0276aa",
+  main: "#03a9f4",
+  dark: "#35baf6"
+};
+
+export const successColors = {
+  light: "#357a38",
+  main: "#4caf50",
+  dark: "#6fbf73"
+};
+
+export const textColors = {
+  primary: "rgba(255, 255, 255, 1)",
+  secondary: "rgba(255, 255, 255, 0.7)",
+  disabled: "rgba(255, 255, 255, 0.2)"
+};
